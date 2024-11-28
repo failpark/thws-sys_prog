bad9f036df509367dd7f45b2dd6aef92631bc8a1
Phedias <daniel.borgs@noxa.de>
2024-11-28 16:42:50 +0100

    feat: conditionals

--- hello.c
+++ hello.c
@@ -3,6 +3,14 @@ int main() {
 	printf("Hello World\n");
 	int num = 10;
 	scanf("%d", &num);
+	// there is no bool type???
+	if (num == 10) {
+		printf("num is 10\n");
+	} else if (num == 11) {
+		printf("num is 11\n");
+	} else if (num) {
+		printf("not zero\n");
+	}
 	printf("Your input was: %d\n", num);
 	return 0;
 }
